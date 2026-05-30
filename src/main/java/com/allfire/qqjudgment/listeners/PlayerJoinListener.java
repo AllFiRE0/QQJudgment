f04d4686998b86e9fc61bfca8e0f41e9ab0b2e5c
AllF1RE <111210671+AllFiRE0@users.noreply.github.com>
2026-05-31 02:40:04 +0300

    Update PlayerJoinListener.java

--- src/main/java/com/allfire/qqjudgment/listeners/PlayerJoinListener.java
+++ src/main/java/com/allfire/qqjudgment/listeners/PlayerJoinListener.java
@@ -22,29 +22,41 @@ public class PlayerJoinListener implements Listener {
     
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+        // Проверяем, включен ли боссбар в конфиге
+        if (!plugin.getConfig().getBoolean("bossbar.enabled", true)) {
+            if (debug) {
+                plugin.getLogger().info("[PlayerJoin] Боссбар выключен в конфиге, пропускаем");
+            }
+            return;
+        }
+        
+        // Проверяем, активна ли судная ночь
+        if (!judgmentManager.isJudgmentActive()) {
+            if (debug) {
+                plugin.getLogger().info("[PlayerJoin] Судная ночь не активна, пропускаем");
+            }
+            return;
+        }
+        
         Player player = event.getPlayer();
         
-        // Если судная ночь активна
-        if (judgmentManager.isJudgmentActive()) {
-            // Показываем прогресс боссбар
-            plugin.getBossBarManager().showProgressBossBar(player);
-            
-            // Устанавливаем правильный прогресс и время
-            int remainingSeconds = judgmentManager.getRemainingSeconds();
-            int totalSeconds = judgmentManager.getTotalSeconds();
-            
-            if (totalSeconds > 0) {
-                float progress = (float) remainingSeconds / totalSeconds;
-                plugin.getBossBarManager().updateProgressForPlayer(player, progress);
-            }
-            
-            String formattedTime = plugin.getStatsManager().formatTime(remainingSeconds);
-            plugin.getBossBarManager().updateTitleForPlayer(player, formattedTime);
-            
-            if (debug) {
-                plugin.getLogger().info("[PlayerJoin] Показан боссбар для " + player.getName() + 
-                    " (осталось " + remainingSeconds + " сек)");
-            }
+        // Показываем прогресс боссбар для игрока
+        plugin.getBossBarManager().showProgressBossBar(player);
+        
+        int remainingSeconds = judgmentManager.getRemainingSeconds();
+        int totalSeconds = judgmentManager.getTotalSeconds();
+        
+        if (totalSeconds > 0) {
+            float progress = (float) remainingSeconds / totalSeconds;
+            plugin.getBossBarManager().updateProgressForPlayer(player, progress);
+        }
+        
+        String formattedTime = plugin.getStatsManager().formatTime(remainingSeconds);
+        plugin.getBossBarManager().updateTitleForPlayer(player, formattedTime);
+        
+        if (debug) {
+            plugin.getLogger().info("[PlayerJoin] Показан боссбар для " + player.getName() + 
+                " (осталось " + remainingSeconds + " сек)");
         }
     }
 }
