43cd26179c74c286069e6830d3af40b2f40141cf
AllF1RE <111210671+AllFiRE0@users.noreply.github.com>
2026-05-30 23:11:21 +0300

    Create QQJudgment.java

---
+++ src/main/java/com/allfire/qqjudgment/QQJudgment.java
@@ -0,0 +1,109 @@
+package com.allfire.qqjudgment;
+
+import com.allfire.qqjudgment.commands.JudgmentCommand;
+import com.allfire.qqjudgment.hooks.WorldGuardHook;
+import com.allfire.qqjudgment.managers.*;
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+
+public final class QQJudgment extends JavaPlugin {
+    
+    private static QQJudgment instance;
+    private BukkitAudiences adventure;
+    private WorldGuardHook worldGuardHook;
+    private JudgmentManager judgmentManager;
+    private BossBarManager bossBarManager;
+    private StatsManager statsManager;
+    private MessageManager messageManager;
+    
+    @Override
+    public void onEnable() {
+        instance = this;
+        adventure = BukkitAudiences.create(this);
+        
+        // Сохранение конфига
+        saveDefaultConfig();
+        
+        // Инициализация хуков
+        worldGuardHook = new WorldGuardHook();
+        if (!worldGuardHook.setup()) {
+            getLogger().log(Level.SEVERE, "WorldGuard не найден! Плагин отключается.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        
+        // Инициализация менеджеров
+        messageManager = new MessageManager(this);
+        statsManager = new StatsManager(this);
+        judgmentManager = new JudgmentManager(this);
+        bossBarManager = new BossBarManager(this);
+        
+        // Регистрация команд
+        getCommand("qqjudgment").setExecutor(new JudgmentCommand(this));
+        
+        // Регистрация слушателей
+        registerListeners();
+        
+        getLogger().info("QQJudgment успешно загружен! Автор: AllF1RE");
+    }
+    
+    private void registerListeners() {
+        try {
+            Class.forName("com.allfire.qqjudgment.listeners.PVPListener")
+                    .getConstructor(QQJudgment.class)
+                    .newInstance(this);
+            Class.forName("com.allfire.qqjudgment.listeners.PlayerActionListener")
+                    .getConstructor(QQJudgment.class)
+                    .newInstance(this);
+            
+            if (getConfig().getBoolean("mob-spawning.enabled", false)) {
+                Class.forName("com.allfire.qqjudgment.listeners.MobSpawnListener")
+                        .getConstructor(QQJudgment.class)
+                        .newInstance(this);
+            }
+        } catch (Exception e) {
+            getLogger().log(Level.WARNING, "Не удалось зарегистрировать слушателей", e);
+        }
+    }
+    
+    @Override
+    public void onDisable() {
+        if (judgmentManager != null && judgmentManager.isJudgmentActive()) {
+            judgmentManager.stopJudgment(false);
+        }
+        if (adventure != null) {
+            adventure.close();
+        }
+        getLogger().info("QQJudgment выключен");
+    }
+    
+    public static QQJudgment getInstance() {
+        return instance;
+    }
+    
+    public BukkitAudiences getAdventure() {
+        return adventure;
+    }
+    
+    public WorldGuardHook getWorldGuardHook() {
+        return worldGuardHook;
+    }
+    
+    public JudgmentManager getJudgmentManager() {
+        return judgmentManager;
+    }
+    
+    public BossBarManager getBossBarManager() {
+        return bossBarManager;
+    }
+    
+    public StatsManager getStatsManager() {
+        return statsManager;
+    }
+    
+    public MessageManager getMessageManager() {
+        return messageManager;
+    }
+}
